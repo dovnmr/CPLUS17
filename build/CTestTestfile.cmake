5b495a57a778247ad6ea555dcbaef235352fab33
dovnmr <dovnmr@gmail.com>
2023-12-26 16:25:02 +0200

    build

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/dovnimratz/Downloads/[OTUS] C++ Developer. Professional (2023)/01. Система сборки. build, test and deploy_/source
+# Build directory: /Users/dovnimratz/Downloads/[OTUS] C++ Developer. Professional (2023)/01. Система сборки. build, test and deploy_/source/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_version "test_version")
+set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "/Users/dovnimratz/Downloads/[OTUS] C++ Developer. Professional (2023)/01. Система сборки. build, test and deploy_/source/CMakeLists.txt;74;add_test;/Users/dovnimratz/Downloads/[OTUS] C++ Developer. Professional (2023)/01. Система сборки. build, test and deploy_/source/CMakeLists.txt;0;")
